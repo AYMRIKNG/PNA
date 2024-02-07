c68ece97e948ce140e450da6003a797dd9e4af55
Aymerick <120599499+LSDORA@users.noreply.github.com>
2024-02-07 17:49:52 +0100

    enfin

--- src/js/sunmoon.js
+++ src/js/sunmoon.js
@@ -1,37 +1,53 @@
 
-    let mainColor = localStorage.getItem('mainColor');
-    let secondeColor = localStorage.getItem('secondeColor');
-    if (mainColor && secondeColor) {
-        document.documentElement.style.setProperty('--main-color', mainColor);
-        document.documentElement.style.setProperty('--seconde', secondeColor);
-    }
 
-    function swapColors() {
-        let rootStyle = getComputedStyle(document.body);
-        let mainColor = rootStyle.getPropertyValue('--main-color');
-        let secondeColor = rootStyle.getPropertyValue('--seconde');
+let mainColor = localStorage.getItem('mainColor');
+let secondeColor = localStorage.getItem('secondeColor');
+let clearValue = localStorage.getItem('clearValue');
+let clear2Value = localStorage.getItem('clear2Value');
+let blindValue = localStorage.getItem('blindValue');
+let blind2Value = localStorage.getItem('blind2Value');
 
+if (mainColor && secondeColor && clearValue && clear2Value && blindValue && blind2Value) {
+    document.documentElement.style.setProperty('--main-color', mainColor);
+    document.documentElement.style.setProperty('--seconde', secondeColor);
+    document.documentElement.style.setProperty('--clear', clearValue);
+    document.documentElement.style.setProperty('--clear2', clear2Value);
+    document.documentElement.style.setProperty('--blind', blindValue);
+    document.documentElement.style.setProperty('--blind2', blind2Value);
+}
 
-        document.documentElement.style.setProperty('--main-color', secondeColor);
-        document.documentElement.style.setProperty('--seconde', mainColor);
-        var iframe = document.getElementById('hor');
-    iframe.src = iframe.src;
-       
-
+function swapColors() {
+    let rootStyle = getComputedStyle(document.body);
     
-            var i = document.getElementById('sun');
+    let mainColor = rootStyle.getPropertyValue('--main-color');
+    let secondeColor = rootStyle.getPropertyValue('--seconde');
+    let clearValue = rootStyle.getPropertyValue('--clear');
+    let clear2Value = rootStyle.getPropertyValue('--clear2');
+    let blindValue = rootStyle.getPropertyValue('--blind');
+    let blind2Value = rootStyle.getPropertyValue('--blind2');
 
-            if(i.classList.contains("fa-sun")){
-                i.classList.replace("fa-sun","fa-moon") 
-            }else if("fa-moon"){
-                i.classList.replace("fa-moon","fa-sun") 
-            }
-          
-        
+    document.documentElement.style.setProperty('--main-color', secondeColor);
+    document.documentElement.style.setProperty('--seconde', mainColor);
+    document.documentElement.style.setProperty('--clear', blindValue);
+    document.documentElement.style.setProperty('--clear2', blind2Value);
+    document.documentElement.style.setProperty('--blind', clearValue);
+    document.documentElement.style.setProperty('--blind2', clear2Value);
 
-        // Enregistrer les couleurs dans le stockage local
-        localStorage.setItem('mainColor', secondeColor);
-        localStorage.setItem('secondeColor', mainColor);
+    var iframe = document.getElementById('hor');
+    iframe.src = iframe.src;
+
+    var i = document.getElementById('sun');
+
+    if(i.classList.contains("fa-sun")) {
+        i.classList.replace("fa-sun","fa-moon");
+    } else if(i.classList.contains("fa-moon")) {
+        i.classList.replace("fa-moon","fa-sun");
     }
 
-    // Au chargement de la page, définir la couleur de fond à partir du stockage loc
+    localStorage.setItem('mainColor', secondeColor);
+    localStorage.setItem('secondeColor', mainColor);
+    localStorage.setItem('clearValue', blindValue);
+    localStorage.setItem('clear2Value', blind2Value);
+    localStorage.setItem('blindValue', clearValue);
+    localStorage.setItem('blind2Value', clear2Value);
+}
