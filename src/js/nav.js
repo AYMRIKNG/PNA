ea086ea37ab5cfa9bf5070b228ebe54bb18c6a13
Aymerick <120599499+LSDORA@users.noreply.github.com>
2024-06-11 01:37:10 +0200

    change all

--- src/js/nav.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('toggle-menu').addEventListener('click', function () {
-    document.querySelector('.main-menu').classList.remove('show');
-    document.querySelector('.sub-menu').classList.toggle('show');
-});
-document.getElementById('untoggle-menu').addEventListener('click', function () {
-    document.querySelector('.sub-menu').classList.remove('show');
-    document.querySelector('.main-menu').classList.toggle('show');
-});
